c59faa3596b96eaf331f8a312884658cbd77dec2
Pete Florence <pete.florence@gmail.com>
2016-11-29 09:49:35 -0500

    test triangle list works

--- src/memory_visualizer_node.cpp
+++ src/memory_visualizer_node.cpp
@@ -3,18 +3,14 @@
 #include <visualization_msgs/Marker.h>
 #include "geometry_msgs/PoseStamped.h"
 #include "geometry_msgs/TwistStamped.h"
-#include <mavros_msgs/AttitudeTarget.h>
-#include <nav_msgs/OccupancyGrid.h>
-#include <sensor_msgs/PointCloud2.h>
+#include "geometry_msgs/Point.h"
 
 #include "tf/tf.h"
 #include <tf2_ros/transform_listener.h>
 #include <tf2_ros/transform_broadcaster.h>
 #include <tf2_geometry_msgs/tf2_geometry_msgs.h>
 
-#include <mutex>
 #include <cmath>
-#include <time.h>
 #include <stdlib.h>
 #include <chrono>
 
@@ -28,7 +24,7 @@ public:
 		pose_sub = nh.subscribe("/pose", 1, &MemoryVisualizerNode::OnPose, this);
 
   	    // Publishers
-		fov_pub = nh.advertise<geometry_msgs::PoseStamped>("/fov", 1);
+		fov_pub = nh.advertise<visualization_msgs::Marker>("fov", 0);
 
 		tf_listener_ = std::make_shared<tf2_ros::TransformListener>(tf_buffer_);
 	}
@@ -38,7 +34,66 @@ private:
 
 	void OnPose( geometry_msgs::PoseStamped const& pose ) {
 		ROS_INFO("GOT POSE");
+		PublishTestMarker();
 	}
+
+	void PublishTestMarker() {
+		visualization_msgs::Marker marker;
+     	marker.header.frame_id = drawing_frame;
+    	marker.header.stamp = ros::Time::now();
+  	   	marker.ns = "fov_namespace";
+	    marker.id = 0;
+  		marker.type = visualization_msgs::Marker::TRIANGLE_LIST;
+  		marker.action = visualization_msgs::Marker::ADD;
+    	marker.pose.position.x = 0.0;
+    	marker.pose.position.y = 0.0;
+  		marker.pose.position.z = 0.0;
+   		marker.pose.orientation.x = 0.0;
+  		marker.pose.orientation.y = 0.0;
+   		marker.pose.orientation.z = 0.0;
+    	marker.pose.orientation.w = 1.0;
+    	marker.pose.position.x = 0.0;
+     	marker.scale.x = 1.0;
+     	marker.scale.y = 1.0;
+     	marker.scale.z = 1.0;
+     	marker.color.r = 1.0;
+     	marker.color.g = 1.0;
+     	marker.color.b = 1.0;
+     	marker.color.a = 1.0;
+    	for (int x = 0; x < 10; ++x) {
+       		for (int y = 0; y < 10; ++y) {
+        		for (int z = 0; z < 10; ++z) {
+     				geometry_msgs::Point p;
+       				p.x = x * 0.1f;
+     				p.y = y * 0.1f;
+	          		p.z = z * 0.1f;
+	 
+	           		geometry_msgs::Point p2 = p;
+	           		p2.x = p.x + 0.05;
+	 
+	           		geometry_msgs::Point p3 = p;
+	           		p3.x = p2.x;
+	           		p3.z = p.z + 0.05;
+	           		marker.points.push_back(p);
+	           		marker.points.push_back(p2);
+	           		marker.points.push_back(p3);
+	 
+	           		std_msgs::ColorRGBA c;
+	           		c.r = x * 0.1;
+	           		c.g = y * 0.1;
+	           		c.b = z * 0.1;
+	           		c.a = 1.0;
+	           		marker.colors.push_back(c);
+	           		marker.colors.push_back(c);
+	          		marker.colors.push_back(c);
+	        	}
+	      	}
+	    }
+	    fov_pub.publish( marker );
+	}
+
+
+	std::string drawing_frame = "r200_depth_optical_frame";
 
 	ros::Subscriber pose_sub;
 
