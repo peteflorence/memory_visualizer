d79967c16cc44aabddb2ee839f6693a11b42967a
Pete Florence <pete.florence@gmail.com>
2016-11-29 10:19:00 -0500

    visualizing frustum

--- src/memory_visualizer_node.cpp
+++ src/memory_visualizer_node.cpp
@@ -1,3 +1,7 @@
+#include <Eigen/Dense>
+typedef float Scalar;
+typedef Eigen::Matrix<Scalar, 3, 1> Vector3;
+
 #include <ros/ros.h>
 #include <nav_msgs/Path.h>
 #include <visualization_msgs/Marker.h>
@@ -34,7 +38,79 @@ private:
 
 	void OnPose( geometry_msgs::PoseStamped const& pose ) {
 		ROS_INFO("GOT POSE");
-		PublishTestMarker();
+		PublishFovMarker();
+	}
+
+	void PublishFovMarker() {
+		visualization_msgs::Marker marker;
+     	marker.header.frame_id = drawing_frame;
+    	marker.header.stamp = ros::Time::now();
+  	   	marker.ns = "fov_namespace";
+	    marker.id = 0;
+  		marker.type = visualization_msgs::Marker::TRIANGLE_LIST;
+  		marker.action = visualization_msgs::Marker::ADD;
+    	marker.pose.position.x = 0.0;
+    	marker.pose.position.y = 0.0;
+  		marker.pose.position.z = 0.0;
+   		marker.pose.orientation.x = 0.0;
+  		marker.pose.orientation.y = 0.0;
+   		marker.pose.orientation.z = 0.0;
+    	marker.pose.orientation.w = 1.0;
+    	marker.pose.position.x = 0.0;
+     	marker.scale.x = 1.0;
+     	marker.scale.y = 1.0;
+     	marker.scale.z = 1.0;
+     	marker.color.r = 1.0;
+     	marker.color.g = 1.0;
+     	marker.color.b = 1.0;
+     	marker.color.a = 1.0;
+
+     	std::vector<Vector3> fov_corners;
+     	fov_corners.push_back(Vector3(5,4,10)); // bottom right
+     	fov_corners.push_back(Vector3(5,-4,10)); // top right
+     	fov_corners.push_back(Vector3(-5,-4,10)); // top left
+     	fov_corners.push_back(Vector3(-5,4,10)); // bottom left  
+
+     	int j = 0;
+     	for (int i = 0; i < 4; i++) {
+     		j = i+1;
+     		if (j == 4) {j = 0;}; // connect back around
+     		BuildSideOfFOV(fov_corners.at(i), fov_corners.at(j), marker);
+     	}
+
+	    fov_pub.publish( marker );
+	}
+
+	void BuildSideOfFOV(Vector3 corner_1, Vector3 corner_2, visualization_msgs::Marker& marker) {
+
+		geometry_msgs::Point p;
+		p.x = 0.0;
+		p.y = 0.0;
+  		p.z = 0.0;
+
+   		geometry_msgs::Point p2 = p;
+   		p2.x = corner_1(0);
+   		p2.y = corner_1(1);
+   		p2.z = corner_1(2);
+
+   		geometry_msgs::Point p3 = p;
+   		p3.x = corner_2(0);
+   		p3.y = corner_2(1);
+   		p3.z = corner_2(2);
+
+   		marker.points.push_back(p);
+   		marker.points.push_back(p2);
+   		marker.points.push_back(p3);
+
+   		std_msgs::ColorRGBA c;
+   		c.r = 1.0;
+   		c.g = 1.0;
+   		c.b = 0.0;
+   		c.a = 0.15;
+   		marker.colors.push_back(c);
+   		marker.colors.push_back(c);
+  		marker.colors.push_back(c);
+	        	    	
 	}
 
 	void PublishTestMarker() {
