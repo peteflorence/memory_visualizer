c1b076c07f03ab7a1e9fbde1d99f499b8cac551c
peteflorence <pete.florence@gmail.com>
2017-01-27 09:53:51 -0500

    just cleaning

--- src/depth_graph_visualizer_node.cpp
+++ src/depth_graph_visualizer_node.cpp
@@ -33,7 +33,6 @@ public:
 		tf_listener_ = std::make_shared<tf2_ros::TransformListener>(tf_buffer_);
 	}
 
-	
 private:
 
   bool initiated = false;
@@ -42,9 +41,30 @@ private:
   Vector3 last_t;
   std::vector<Eigen::Matrix4d> odometries;
 
-  // delete this later
-  Eigen::Matrix4d current_transform;
-
+  void OnPose( geometry_msgs::PoseStamped const& pose ) {
+    if (!initiated) {
+      last_R = constructR(pose);
+      last_t = constructt(pose);
+      initiated = true;
+      Eigen::Matrix4d transform_identity = Eigen::Matrix4d::Identity();
+      for (int i = 0; i < 20; i++) {
+        odometries.push_back(transform_identity);
+      }
+      return;
+    }
+    
+    counter++;
+    if (counter >= 25) {
+      counter = 0;
+      ROS_INFO("GOT POSE");
+      Matrix3 R = constructR(pose);
+      Vector3 t = constructt(pose);
+      AddToOdometries(findTransform(R, t, last_R, last_t));
+      last_R = R;
+      last_t = t;
+      PublishFovMarkers();
+    }
+  }
 
   Matrix3 constructR( geometry_msgs::PoseStamped const& pose ) {
     Eigen::Quaternion<Scalar> quat(pose.pose.orientation.x, pose.pose.orientation.y, pose.pose.orientation.z, pose.pose.orientation.w);
@@ -52,66 +72,26 @@ private:
   }
 
   Vector3 constructt( geometry_msgs::PoseStamped const& pose ) {
-    Vector3 t;
-    t << pose.pose.position.x, pose.pose.position.y, pose.pose.position.z;
-    return t;
+    return Vector3(pose.pose.position.x, pose.pose.position.y, pose.pose.position.z);
   }
 
   Eigen::Matrix4d findTransform(Matrix3 R_1, Vector3 t_1, Matrix3 R_2, Vector3 t_2) {
-    // T_1^W inverse
     Eigen::Matrix4d transform_1 = Eigen::Matrix4d::Identity();
     transform_1.block<3,3>(0,0) = R_1.transpose();
-    transform_1.block<3,1>(0,3) = -1.0 * R_1.transpose() * t_1;
+    transform_1.block<3,1>(0,3) = -1.0 * R_1.transpose() * t_1; // T_1^W inverse
 
     Eigen::Matrix4d transform_2 = Eigen::Matrix4d::Identity();
     transform_2.block<3,3>(0,0) = R_2;
-    transform_2.block<3,1>(0,3) = t_2;
+    transform_2.block<3,1>(0,3) = t_2; // T_2^W
 
     return transform_1*transform_2;
   }
 
   void AddToOdometries(Eigen::Matrix4d current_transform) {
-    if (odometries.size() < 20) {
-      std::cout << "building up " << odometries.size();
-      odometries.push_back(current_transform);
-      return;
-    } 
-
     std::cout << "rotate and add" << std::endl;
-    // Shift vector so each move back 1
-    rotate(odometries.begin(),odometries.end()-1,odometries.end());
+    rotate(odometries.begin(),odometries.end()-1,odometries.end()); // Shift vector so each move back 1
     odometries.at(0) = current_transform;
-
   }
-
-
-	void OnPose( geometry_msgs::PoseStamped const& pose ) {
-    if (!initiated) {
-      last_R = constructR(pose);
-      last_t = constructt(pose);
-      current_transform = Eigen::Matrix4d::Identity();
-      initiated = true;
-      return;
-    }
-    
-
-		counter++;
-		if (counter >= 25) {
-			counter = 0;
-			ROS_INFO("GOT POSE");
-      Matrix3 R = constructR(pose);
-      Vector3 t = constructt(pose);
-      
-      current_transform = findTransform(R, t, last_R, last_t);
-      AddToOdometries(current_transform);
-      last_R = R;
-      last_t = t;
-			
-			PublishFovMarkers();
-      //PrintToyTransform();
-      //PrintVectorPractice();
-		}
-	}
 
   void PublishFovMarkers() {
     for (int fov_id = 0; fov_id < odometries.size(); fov_id++) {
@@ -119,76 +99,60 @@ private:
     }
   }
 
-  Vector3 transformToCurrentRDFframe(Vector3 position_other_rdf_frame, int fov_id) {
-    Eigen::Matrix4d transform_2 = Eigen::Matrix4d::Identity();
-
-    Vector4 position_current_rdf_frame;
-    position_current_rdf_frame << position_other_rdf_frame(0), position_other_rdf_frame(1), position_other_rdf_frame(2), 1.0;
-
-    for (int i = 0; i < fov_id; i++) {
-      position_current_rdf_frame = odometries.at(i) * position_current_rdf_frame;
-    }
-
-    Vector3 answer;
-    answer << position_current_rdf_frame(0), position_current_rdf_frame(1), position_current_rdf_frame(2);
-    return answer;
-  }
-
 	void PublishFovMarker(int fov_id) {
 		visualization_msgs::Marker marker;
-     	marker.header.frame_id = drawing_frame;
-    	marker.header.stamp = ros::Time::now();
-  	   	marker.ns = "fov_side";
-	    marker.id = fov_id;
-  		marker.type = visualization_msgs::Marker::TRIANGLE_LIST;
-  		marker.action = visualization_msgs::Marker::ADD;
-      Vector3 p = transformToCurrentRDFframe(Vector3(0,0,0), fov_id);
+    marker.header.frame_id = drawing_frame;
+    marker.header.stamp = ros::Time::now();
+  	marker.ns = "fov_side";
+	  marker.id = fov_id;
+  	marker.type = visualization_msgs::Marker::TRIANGLE_LIST;
+  	marker.action = visualization_msgs::Marker::ADD;
+    Vector3 p = transformToCurrentRDFframe(Vector3(0,0,0), fov_id);
 
-    	marker.pose.position.x = p(0);
-    	marker.pose.position.y = p(1);
-  		marker.pose.position.z = p(2);
-   		marker.pose.orientation.x = 0.0;
-  		marker.pose.orientation.y = 0.0;
-   		marker.pose.orientation.z = 0.0;
-    	marker.pose.orientation.w = 1.0;
-     	marker.scale.x = 1.0;
-     	marker.scale.y = 1.0;
-     	marker.scale.z = 1.0;
-     	marker.color.r = 1.0;
-     	marker.color.g = 1.0;
-     	marker.color.b = 1.0;
-     	marker.color.a = 1.0;
+  	marker.pose.position.x = p(0);
+  	marker.pose.position.y = p(1);
+		marker.pose.position.z = p(2);
+ 		marker.pose.orientation.x = 0.0;
+		marker.pose.orientation.y = 0.0;
+ 		marker.pose.orientation.z = 0.0;
+  	marker.pose.orientation.w = 1.0;
+   	marker.scale.x = 1.0;
+   	marker.scale.y = 1.0;
+   	marker.scale.z = 1.0;
+   	marker.color.r = 1.0;
+   	marker.color.g = 1.0;
+   	marker.color.b = 1.0;
+   	marker.color.a = 1.0;
 
-     	std::vector<Vector3> fov_corners;
-     	fov_corners.push_back(transformToCurrentRDFframe(Vector3(7,5.25,10),fov_id)); // bottom right
-     	fov_corners.push_back(transformToCurrentRDFframe(Vector3(7,-5.25,10),fov_id)); // top right
-     	fov_corners.push_back(transformToCurrentRDFframe(Vector3(-7,-5.25,10),fov_id)); // top left
-     	fov_corners.push_back(transformToCurrentRDFframe(Vector3(-7,5.25,10),fov_id)); // bottom left  
+   	std::vector<Vector3> fov_corners;
+   	fov_corners.push_back(transformToCurrentRDFframe(Vector3(7,5.25,10),fov_id)); // bottom right
+   	fov_corners.push_back(transformToCurrentRDFframe(Vector3(7,-5.25,10),fov_id)); // top right
+   	fov_corners.push_back(transformToCurrentRDFframe(Vector3(-7,-5.25,10),fov_id)); // top left
+   	fov_corners.push_back(transformToCurrentRDFframe(Vector3(-7,5.25,10),fov_id)); // bottom left  
 
-     	int j = 0;
-     	for (int i = 0; i < 4; i++) {
-     		j = i+1;
-     		if (j == 4) {j = 0;}; // connect back around
-     		BuildSideOfFOV(fov_corners.at(i), fov_corners.at(j), marker, fov_id);
-     	}
-     	fov_pub.publish( marker );
+   	int j = 0;
+   	for (int i = 0; i < 4; i++) {
+   		j = i+1;
+   		if (j == 4) {j = 0;}; // connect back around
+   		BuildSideOfFOV(fov_corners.at(i), fov_corners.at(j), marker, fov_id);
+   	}
+   	fov_pub.publish( marker );
 
-     	marker.type = visualization_msgs::Marker::LINE_LIST;
-     	marker.ns = "fov_line";
-     	marker.scale.x = 0.1;
-     	marker.scale.y = 0.1;
-     	marker.scale.z = 0.1;
-     	marker.points.clear();
-     	marker.colors.clear();
-     	for (int i = 0; i < 4; i++) {
-     		j = i+1;
-     		if (j == 4) {j = 0;}; // connect back around
-     		BuildLineOfFOV(transformToCurrentRDFframe(Vector3(0,0,0),fov_id), fov_corners.at(i), marker, fov_id);
-     		BuildLineOfFOV(fov_corners.at(i), fov_corners.at(j), marker, fov_id);
-     	}
-     	fov_pub.publish( marker );
-
-	    
+   	marker.type = visualization_msgs::Marker::LINE_LIST;
+   	marker.ns = "fov_line";
+   	marker.scale.x = 0.1;
+   	marker.scale.y = 0.1;
+   	marker.scale.z = 0.1;
+   	marker.points.clear();
+   	marker.colors.clear();
+   	for (int i = 0; i < 4; i++) {
+   		j = i+1;
+   		if (j == 4) {j = 0;}; // connect back around
+   		BuildLineOfFOV(transformToCurrentRDFframe(Vector3(0,0,0),fov_id), fov_corners.at(i), marker, fov_id);
+   		BuildLineOfFOV(fov_corners.at(i), fov_corners.at(j), marker, fov_id);
+   	}
+   	fov_pub.publish( marker );
+  
 	}
 
 	void BuildSideOfFOV(Vector3 corner_1, Vector3 corner_2, visualization_msgs::Marker& marker, int fov_id) {
@@ -262,6 +226,21 @@ private:
    		marker.colors.push_back(c);
    		marker.colors.push_back(c);      	    	
 	}
+
+  Vector3 transformToCurrentRDFframe(Vector3 position_other_rdf_frame, int fov_id) {
+    Eigen::Matrix4d transform_2 = Eigen::Matrix4d::Identity();
+
+    Vector4 position_current_rdf_frame;
+    position_current_rdf_frame << position_other_rdf_frame(0), position_other_rdf_frame(1), position_other_rdf_frame(2), 1.0;
+
+    for (int i = 0; i < fov_id; i++) {
+      position_current_rdf_frame = odometries.at(i) * position_current_rdf_frame;
+    }
+
+    Vector3 answer;
+    answer << position_current_rdf_frame(0), position_current_rdf_frame(1), position_current_rdf_frame(2);
+    return answer;
+  }
 
 	Vector3 TransformRDFtoWorld(Vector3 const& ortho_body_frame) {
 		geometry_msgs::TransformStamped tf;
